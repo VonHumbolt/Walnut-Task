25b88c6562733ee8a7a797a678b6e31ca7e84f51
VonHumbolt <kaankaplan111@gmail.com>
2025-03-12 21:53:25 +0300

    init project

---
+++ walnut-news/src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require('cors')
+
+const app = express();
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log("Listening on port -> ", PORT)
+})
